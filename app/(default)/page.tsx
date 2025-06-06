df5be5fb3287b071d2de2199caefaeec9db6d88a
ningyongheng <ningyongheng@jeejio.com>
2025-06-06 15:52:03 +0800

    feat: 修改当前的title展示和文案

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,14 +1,6 @@
-/*
- * @Author: ningyongheng ningyongheng@jeejio.com
- * @Date: 2024-08-08 19:19:52
- * @LastEditors: ningyongheng ningyongheng@jeejio.com
- * @LastEditTime: 2024-09-02 10:40:51
- * @FilePath: /tailwind-landing-page-template/app/(default)/page.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 export const metadata = {
-  title: "Solaris",
-  description: "下一代智能AI应用",
+  title: "world",
+  description: "world see see",
 };
 
 import HomeMap from "@/components/map/home";
@@ -18,10 +10,10 @@ export default function Home() {
     <>
       <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
         <h1 className="text-4xl font-bold text-white mb-4">
-          Welcome to Solaris
+          Welcome to World
         </h1>
         <HomeMap />
-        <p className="text-lg text-white mb-8">Your AI-powered companion</p>
+        <p className="text-lg text-white mb-8">traval china, travel world</p>
       </div>
     </>
   );
